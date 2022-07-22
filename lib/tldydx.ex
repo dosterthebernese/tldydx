386066b51958f767466d4a36f492d8137b9a9be5
neil <orderllrobbieesquire@gmail.com>
2022-07-22 09:07:02 -0400

    cleaned up get_dydx, need to update readme

--- lib/tldydx.ex
+++ lib/tldydx.ex
@@ -142,14 +142,21 @@ defmodule TLDYDX do
   end
 
   # if you don't pass it a proper date, and default to now, it'll always error, as the future numbers are not in place.
-  def get_dydx(asset_pair, {:ok, ltdate} \\ DateTime.now("Etc/UTC")) do
+  def get_dydx(
+        asset_pair,
+        iterate_range \\ @seconds30min,
+        back_and_forward_range \\ @seconds10min,
+        {:ok, ltdateraw} \\ DateTime.now("Etc/UTC")
+      ) do
+    #    back_and_forward_range_halved = @seconds10min / 2
+    # need as integer dummy
+    back_and_forward_range_halved = div(@seconds10min, 2)
+    ltdate = DateTime.add(ltdateraw, -(back_and_forward_range + @a_little_sumpin), :second)
+
     {:ok, pid} = Postgrex.start_link(@pgcreds)
-    ### go back from start 24 hours for your rows to iterate
-    gtedate = DateTime.add(ltdate, -@seconds24h, :second)
-    ### for max, following 30 mins for data plus a lil sumpin
-    subservient_ltdate = DateTime.add(ltdate, @seconds30min + @a_little_sumpin, :second)
-    ### for min, prior 2 hours for data
-    subservient_gtedate = DateTime.add(gtedate, -@seconds2h, :second)
+    gtedate = DateTime.add(ltdate, -iterate_range, :second)
+    subservient_ltdate = DateTime.add(ltdate, back_and_forward_range + @a_little_sumpin, :second)
+    subservient_gtedate = DateTime.add(gtedate, -back_and_forward_range, :second)
 
     IO.puts("  The bottom of the range to iterate is:  #{gtedate}")
     IO.puts("     The top of the range to iterate is:  #{ltdate}")
@@ -164,9 +171,9 @@ defmodule TLDYDX do
     subservient_quotes = get_dydx_range(pid, asset_pair, subservient_gtedate, subservient_ltdate)
     IO.puts("subservient number of rows: " <> " " <> "#{inspect(subservient_quotes.num_rows)}")
 
-    # need the 24 hours you are iterating, plus the 30 mins forward and 2 hours back
+    # need the mins you are iterating, plus the forward and back
     if subservient_quotes.num_rows <
-         quotes.num_rows + @seconds30min + @seconds2h - @margin_of_error do
+         quotes.num_rows + back_and_forward_range + back_and_forward_range - @margin_of_error do
       raise "Oh no, not enough data yet!"
     end
 
@@ -187,132 +194,66 @@ defmodule TLDYDX do
       # IO.inspect(prior_five_prices)
       # IO.inspect(following_five_prices)
 
-      # its an index from the iterated rows, but you want to start at the backend of the subservient rows
-      trailing_rows_2h = Enum.slice(subservient_quotes.rows, index, @seconds2h)
-      trailing_rows_1h = Enum.slice(subservient_quotes.rows, index + @seconds1h, @seconds1h)
+      trailing_rows_bigger = Enum.slice(subservient_quotes.rows, index, back_and_forward_range)
 
-      future_10min_price =
-        Enum.at(Enum.at(subservient_quotes.rows, index + @seconds2h + @seconds10min), 1)
+      IO.puts(index + back_and_forward_range_halved)
 
-      future_30min_price =
-        Enum.at(Enum.at(subservient_quotes.rows, index + @seconds2h + @seconds30min), 1)
+      trailing_rows_halved =
+        Enum.slice(
+          subservient_quotes.rows,
+          index + back_and_forward_range_halved,
+          back_and_forward_range_halved
+        )
 
-      index_prices_last_2_hours = Enum.map(trailing_rows_2h, &Enum.at(&1, 1))
-      index_prices_last_1_hour = Enum.map(trailing_rows_1h, &Enum.at(&1, 1))
-      stats_map2h = Statistex.statistics(index_prices_last_2_hours)
-      stats_map1h = Statistex.statistics(index_prices_last_1_hour)
+      future_halved =
+        Enum.at(
+          Enum.at(
+            subservient_quotes.rows,
+            index + back_and_forward_range + back_and_forward_range_halved
+          ),
+          1
+        )
 
-      delta_10min = (future_10min_price - parent_price) / parent_price * 100.0
-      delta_30min = (future_30min_price - parent_price) / parent_price * 100.0
+      future_bigger =
+        Enum.at(
+          Enum.at(
+            subservient_quotes.rows,
+            index + back_and_forward_range + back_and_forward_range
+          ),
+          1
+        )
 
-      Postgrex.query(
-        pid,
-        "INSERT INTO dydxd (dydx_id, trailing_2h_average, trailing_2h_standard_deviation, trailing_2h_variance, trailing_2h_sample_size, trailing_1h_average, trailing_1h_standard_deviation, trailing_1h_variance, trailing_1h_sample_size, future_10min_price, future_30min_price, delta_10min, delta_30min) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13)",
-        [
-          parent_id,
-          stats_map2h.average,
-          stats_map2h.standard_deviation,
-          stats_map2h.variance,
-          stats_map2h.sample_size,
-          stats_map1h.average,
-          stats_map1h.standard_deviation,
-          stats_map1h.variance,
-          stats_map1h.sample_size,
-          future_10min_price,
-          future_30min_price,
-          delta_10min,
-          delta_30min
-        ]
-      )
-    end
+      index_prices_bigger = Enum.map(trailing_rows_bigger, &Enum.at(&1, 1))
+      index_prices_halved = Enum.map(trailing_rows_halved, &Enum.at(&1, 1))
+      stats_map_bigger = Statistex.statistics(index_prices_bigger)
+      stats_map_halved = Statistex.statistics(index_prices_halved)
 
-    Enum.each(Enum.with_index(quotes.rows), &pp_row.(&1))
+      delta_bigger = (future_bigger - parent_price) / parent_price * 100.0
+      delta_halved = (future_halved - parent_price) / parent_price * 100.0
 
-    Process.exit(pid, :shutdown)
-  end
+      pres =
+        Postgrex.query(
+          pid,
+          "INSERT INTO dydxd (dydx_id, bfr, trailing_average, trailing_standard_deviation, trailing_variance, trailing_sample_size, trailing_halved_average, trailing_halved_standard_deviation, trailing_halved_variance, trailing_halved_sample_size, future_halved_price, future_price, delta, delta_halved) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14)",
+          [
+            parent_id,
+            back_and_forward_range,
+            stats_map_bigger.average,
+            stats_map_bigger.standard_deviation,
+            stats_map_bigger.variance,
+            stats_map_bigger.sample_size,
+            stats_map_halved.average,
+            stats_map_halved.standard_deviation,
+            stats_map_halved.variance,
+            stats_map_halved.sample_size,
+            future_halved,
+            future_bigger,
+            delta_halved,
+            delta_bigger
+          ]
+        )
 
-  # if you don't pass it a proper date, and default to now, it'll always error, as the future numbers are not in place.
-  def get_dydx_min(asset_pair, {:ok, ltdate} \\ DateTime.now("Etc/UTC")) do
-    {:ok, pid} = Postgrex.start_link(@pgcreds)
-    ### go back from start 30 mins for your rows to iterate
-    gtedate = DateTime.add(ltdate, -@seconds30min, :second)
-    ### for max, following 10 mins for data
-    subservient_ltdate = DateTime.add(ltdate, @seconds10min + @a_little_sumpin, :second)
-    ### for min, prior 10 mins for data
-    subservient_gtedate = DateTime.add(gtedate, -@seconds10min, :second)
-
-    IO.puts("  The bottom of the range to iterate is:  #{gtedate}")
-    IO.puts("     The top of the range to iterate is:  #{ltdate}")
-    IO.puts("The bottom of the lookback for stats is:  #{subservient_gtedate}")
-    IO.puts("  The top of the lookahead for stats is:  #{subservient_ltdate}")
-
-    # you are going to iterate this
-    quotes = get_dydx_range(pid, asset_pair, gtedate, ltdate)
-    IO.puts("            number of rows: " <> " " <> "#{inspect(quotes.num_rows)}")
-
-    # rather than hit the database per each quote above, get the widest range you need, and then "walk" the appropriate ranges.
-    subservient_quotes = get_dydx_range(pid, asset_pair, subservient_gtedate, subservient_ltdate)
-    IO.puts("subservient number of rows: " <> " " <> "#{inspect(subservient_quotes.num_rows)}")
-
-    # need the 30 mins you are iterating, plus the 10 forward and 10 back
-    if subservient_quotes.num_rows <
-         quotes.num_rows + @seconds10min + @seconds10min - @margin_of_error do
-      raise "Oh no, not enough data yet!"
-    end
-
-    ### un comment me when you're feeling dumb
-    #    raise "for now just read the stats above and grok"
-
-    pp_row = fn rowwi ->
-      {row, index} = rowwi
-      IO.inspect(row)
-
-      parent_id = Enum.at(row, 0)
-      parent_price = Enum.at(row, 1)
-
-      ### this stuff is fun to sanity check yourself...the time in current row should sandwich
-      # IO.puts("#{parent_id} #{parent_price} #{index}")
-      # prior_five_prices = Enum.slice(subservient_quotes.rows, index - 5, 5)
-      # following_five_prices = Enum.slice(subservient_quotes.rows, index + 1, 5)
-      # IO.inspect(prior_five_prices)
-      # IO.inspect(following_five_prices)
-
-      trailing_rows_10 = Enum.slice(subservient_quotes.rows, index, @seconds10min)
-      trailing_rows_5 = Enum.slice(subservient_quotes.rows, index + @seconds5min, @seconds5min)
-
-      future_5min_price =
-        Enum.at(Enum.at(subservient_quotes.rows, index + @seconds10min + @seconds5min), 1)
-
-      future_10min_price =
-        Enum.at(Enum.at(subservient_quotes.rows, index + @seconds10min + @seconds10min), 1)
-
-      index_prices_last_10_mins = Enum.map(trailing_rows_10, &Enum.at(&1, 1))
-      index_prices_last_5_mins = Enum.map(trailing_rows_5, &Enum.at(&1, 1))
-      stats_map10min = Statistex.statistics(index_prices_last_10_mins)
-      stats_map5min = Statistex.statistics(index_prices_last_5_mins)
-
-      delta_5min = (future_5min_price - parent_price) / parent_price * 100.0
-      delta_10min = (future_10min_price - parent_price) / parent_price * 100.0
-
-      Postgrex.query(
-        pid,
-        "INSERT INTO dydxdmin (dydx_id, trailing_10min_average, trailing_10min_standard_deviation, trailing_10min_variance, trailing_10min_sample_size, trailing_5min_average, trailing_5min_standard_deviation, trailing_5min_variance, trailing_5min_sample_size, future_5min_price, future_10min_price, delta_5min, delta_10min) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13)",
-        [
-          parent_id,
-          stats_map10min.average,
-          stats_map10min.standard_deviation,
-          stats_map10min.variance,
-          stats_map10min.sample_size,
-          stats_map5min.average,
-          stats_map5min.standard_deviation,
-          stats_map5min.variance,
-          stats_map5min.sample_size,
-          future_5min_price,
-          future_10min_price,
-          delta_5min,
-          delta_10min
-        ]
-      )
+      IO.inspect(pres)
     end
 
     Enum.each(Enum.with_index(quotes.rows), &pp_row.(&1))
@@ -327,19 +268,10 @@ defmodule TLDYDX do
       Postgrex.prepare!(
         pid,
         "",
-        "CREATE TABLE dydxd (dydx_id int references dydx(id), trailing_2h_average float, trailing_2h_standard_deviation float, trailing_2h_variance float, trailing_2h_sample_size integer, trailing_1h_average float, trailing_1h_standard_deviation float, trailing_1h_variance float, trailing_1h_sample_size integer, future_10min_price float, future_30min_price float, delta_10min float, delta_30min float)"
+        "CREATE TABLE dydxd (dydx_id int references dydx(id), bfr int, trailing_average float, trailing_standard_deviation float, trailing_variance float, trailing_sample_size integer, trailing_halved_average float, trailing_halved_standard_deviation float, trailing_halved_variance float, trailing_halved_sample_size integer, future_price float, future_halved_price float, delta float, delta_halved float)"
       )
 
     Postgrex.execute(pid, query1, [])
-
-    query2 =
-      Postgrex.prepare!(
-        pid,
-        "",
-        "CREATE TABLE dydxdmin (dydx_id int references dydx(id), trailing_10min_average float, trailing_10min_standard_deviation float, trailing_10min_variance float, trailing_10min_sample_size integer, trailing_5min_average float, trailing_5min_standard_deviation float, trailing_5min_variance float, trailing_5min_sample_size integer, future_5min_price float, future_10min_price float, delta_5min float, delta_10min float)"
-      )
-
-    Postgrex.execute(pid, query2, [])
   end
 
   def build_database() do
@@ -367,8 +299,6 @@ defmodule TLDYDX do
 
     query1 = Postgrex.prepare!(pid, "", "DROP TABLE dydxd")
     Postgrex.execute(pid, query1, [])
-    query2 = Postgrex.prepare!(pid, "", "DROP TABLE dydxdmin")
-    Postgrex.execute(pid, query2, [])
   end
 
   def optimize_database() do
